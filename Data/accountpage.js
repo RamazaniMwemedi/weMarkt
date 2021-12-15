f9fb05bc24be4103559eb6ca3095dbf0208d1496
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2021-12-15 17:03:55 +0300

    Edited the account page
    I edited the account page but there is a bud in the gid styling

---
+++ Data/accountpage.js
@@ -0,0 +1,59 @@
+export default[
+    {
+        id:1,
+        title:"Your Orders",
+        description:"Track, return, or buy something again",
+        url:"#",
+        image:""
+    },
+    {
+        id:2,
+        title:"Login & Seccurity",
+        description:"Edit login, name, mobile number",
+        url:"#",
+        image:""
+    },
+    {
+        id:3,
+        title:"Gift Card",
+        description:"View balance, redeem or reload cards",
+        url:"#",
+        image:""
+    },
+    {
+        id:4,
+        title:"Your Payment",
+        description:"Manage your payment methods and settings view all transactions",
+        url:"#",
+        image:""
+    },
+    {
+        id:5,
+        title:"Your Profile",
+        description:"Manage, add or remove profiles for personalized experiences",
+        url:"#",
+        image:""
+    },
+    {
+        id:6,
+        title:"Your Messages",
+        description:"View messages to, and from weMarkt, sellers and buyers",
+        url:"#",
+        image:""
+    },
+    {
+        id:7,
+        title:"Achived Orders",
+        description:"View and manage your achived orders",
+        url:"#",
+        image:""
+    },
+    {
+        id:8,
+        title:"Your list",
+        description:"View, modify, and share your list, or create new ones",
+        url:"#",
+        image:""
+    },
+   
+]
--- pages/account.jsx
+++ pages/account.jsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
+
 import styles from "../styles/account.module.css";
+import Datas from '../Data/accountpage'
 import Header from "../Components/Header";
 function account() {
   return (
@@ -9,7 +11,17 @@ function account() {
       </Head>
       <Header />
       <div className={styles.account}>
-        <h2>The account</h2>
+        {Datas.map((data)=>{
+          const {id,title, description, url}=data
+          return(
+            <div className={styles.items}>
+              <div className={styles.item}>
+                <h4>{title}</h4>
+                <p>{description}</p>
+              </div>
+            </div>
+          )
+        })} 
       </div>
     </>
   );
